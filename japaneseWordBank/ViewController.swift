1d046cbc4cd1d1cad8e6abd543a587cb1945df7a
Jack Mooltham <sharkjack11@gmail.com>
2017-05-19 00:04:20 +0800

    Initial Commit

---
+++ japaneseWordBank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  japaneseWordBank
+//
+//  Created by RAYW2 on 19/5/2017.
+//  Copyright © 2017年 RAYW2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
